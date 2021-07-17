0d446ea4ca06efc538cb8168eb66df36b2b4661d
manga-sab <sabyashiv@gmail.com>
2021-07-16 21:02:25 -0400

    Update 4d likelihood computation

--- model/get_4d_ll.m
+++ model/get_4d_ll.m
@@ -1,5 +1,5 @@
 sigs=logspace(-1,2,51);
-nsamps=[1:2:100];
+nsamps=[1,2,3,5,10,Inf];
 lls=zeros(512,16,numel(nsamps),numel(sigs));
 for i=1:512
     i
@@ -9,7 +9,7 @@ for i=1:512
             for l=1:numel(nsamps)
             
             dmat=sign((dec2bin(i-1,9)-'0')-0.5);
-            lls(i,:,l,k)=ll_fixedsampling_v3([nsamps(l),sigs(k)],dmat,10000);
+            lls(i,:,l,k)=ll_fixedsampling_v3([nsamps(l),sigs(k)],dmat,40000);
             
             end
         end
